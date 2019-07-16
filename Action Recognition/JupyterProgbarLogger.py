169eff7e0c8d20feaff6bdeab71723fb6ed9e283
AmitPr <17amitprasad2gmail.com>
2019-07-16 12:44:52 -0400

    :moyai:

---
+++ Action Recognition/JupyterProgbarLogger.py
@@ -0,0 +1,114 @@
+from tensorflow.keras.callbacks import Callback
+from tqdm import tqdm_notebook as tqdm
+from collections import OrderedDict
+import numpy as np
+class JupyterProgbarLogger(Callback):
+    """Callback that prints metrics to stdout. -- Fixed for Jupyter Notebooks
+    # Arguments
+        count_mode: One of "steps" or "samples".
+            Whether the progress bar should
+            count samples seen or steps (batches) seen.
+        stateful_metrics: Iterable of string names of metrics that
+            should *not* be averaged over an epoch.
+            Metrics in this list will be logged as-is.
+            All others will be averaged over time (e.g. loss, etc).
+    # Raises
+        ValueError: In case of invalid `count_mode`.
+    """
+
+    def __init__(self, count_mode='samples',
+                 stateful_metrics=None):
+        super(JupyterProgbarLogger,self).__init__()
+        self._values = OrderedDict()
+        self._seen_so_far = 0
+        if count_mode == 'samples':
+            self.use_steps = False
+        elif count_mode == 'steps':
+            self.use_steps = True
+        else:
+            raise ValueError('Unknown `count_mode`: ' + str(count_mode))
+        if stateful_metrics:
+            self.stateful_metrics = set(stateful_metrics)
+        else:
+            self.stateful_metrics = set()
+            
+    def on_train_begin(self, logs=None):
+        self.verbose = True
+        self.epochs = self.params['epochs']
+
+    def on_epoch_begin(self, epoch, logs=None):
+        self._values = OrderedDict()
+        self._seen_so_far = 0
+        if self.verbose:
+            print('Epoch %d/%d' % (epoch + 1, self.epochs))
+            if self.use_steps:
+                target = self.params['steps']
+            else:
+                target = self.params['samples']
+            self.target = target
+            self.progbar = tqdm(total=self.target)#JupyterProgbar.JupyterProgbar(target=self.target,
+                            #       verbose=self.verbose,
+                             #      stateful_metrics=self.stateful_metrics)
+        self.seen = 0
+
+    def on_batch_begin(self, batch, logs=None):
+        if self.seen < self.target:
+            self.log_values = []
+
+    def on_batch_end(self, batch, logs=None):
+        logs = logs or {}
+        batch_size = logs.get('size', 0)
+        if self.use_steps:
+            self.seen = 1
+        else:
+            self.seen = batch_size
+
+        for k in self.params['metrics']:
+            if k in logs:
+                self.log_values.append((k, logs[k]))
+
+        # Skip progbar update for the last batch;
+        # will be handled by on_epoch_end.
+        if self.verbose and self.seen < self.target:
+            self.progbar.update(self.seen)
+            print(self.update_vals(self.log_values,self.seen),end='\r')
+            #self.progbar.set_description(self.update_vals(self.log_values,self.seen))
+
+    def on_epoch_end(self, epoch, logs=None):
+        logs = logs or {}
+        for k in self.params['metrics']:
+            if k in logs:
+                self.log_values.append((k, logs[k]))
+        if self.verbose:
+                self.progbar.update(self.seen)
+                print(self.update_vals(self.log_values,self.seen),end='\r')
+                #self.progbar.set_description(self.update_vals(self.log_values,self.seen))
+    def update_vals(self,values,step_amt):
+        current=self._seen_so_far+step_amt
+        for k, v in values:
+            if k not in self.stateful_metrics:
+                if k not in self._values:
+                    self._values[k] = [v * (current - self._seen_so_far),
+                                       current - self._seen_so_far]
+                else:
+                    self._values[k][0] += v * (current - self._seen_so_far)
+                    self._values[k][1] += (current - self._seen_so_far)
+            else:
+                # Stateful metrics output a numeric value.  This representation
+                # means "take an average from a single value" but keeps the
+                # numeric formatting.
+                self._values[k] = [v, 1]
+        info=''
+        for k in self._values:
+                info += ' - %s:' % k
+                if isinstance(self._values[k], list):
+                    avg = np.mean(
+                        self._values[k][0] / max(1, self._values[k][1]))
+                    if abs(avg) > 1e-3:
+                        info += ' %.4f' % avg
+                    else:
+                        info += ' %.4e' % avg
+                else:
+                    info += ' %s' % self._values[k]
+        self._seen_so_far =current
+        return info
