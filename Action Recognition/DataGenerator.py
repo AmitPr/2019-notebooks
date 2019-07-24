f06d4d559ee5e6dc04a38ded942ba0dc3a61fa35
Prasad <ap487@login01.o2.rc.hms.harvard.edu>
2019-07-24 14:15:36 -0400

    sync :moyai:

--- Action Recognition/DataGenerator.py
+++ Action Recognition/DataGenerator.py
@@ -18,13 +18,14 @@ class DataGenerator(keras.utils.Sequence):
         self.batch_size = batch_size
         self.frames_per_sample = frames_per_sample
         self.offset=offset
+        self.skipped_frames=0
 
     def __len__(self):
         #How many batches in 1 epoch
-        return int(np.floor((self.data_amount/self.frames_per_sample)/self.batch_size/10))
+        return int(np.floor((self.data_amount/self.frames_per_sample)/self.batch_size))
 
     def __getitem__(self, index):
-        index*=self.batch_size*self.frames_per_sample
+        index*=self.batch_size*self.frames_per_sample+self.skipped_frames
         #Create a batch
         with h5py.File(self.file_path,'r') as f:
             if not self.frames_per_sample == 1:
@@ -41,6 +42,8 @@ class DataGenerator(keras.utils.Sequence):
                         batch_data[i]=f["/frames/raw"][index]
                         batch_labels[i]=f["/labels"][index]
                         i+=1
+                    else:
+                        self.skipped_frames+=1
                 return batch_data[...,None],batch_labels
 
 
@@ -69,56 +72,5 @@ class DataGenerator(keras.utils.Sequence):
             return chunk_data[...,None],chunk_labels
     
     def on_epoch_end(self):
+        self.skipped_frames=0
         pass
-
-    
-#Reprocess a file for the data generator. -- used for old Data Generator
-#This is done in advance so that python doesn't run out of memory when using multiprocessing.
-def process_file(in_file_path, out_file_path,frames_per_sample=32, data_amount=300000,frame_shape=(80,80,1)):
-    with h5py.File(in_file_path,'r') as f:
-            #Load
-            labels = (f['/labels'][:data_amount])
-            data_amount=len(labels)
-            combine_frames=(not frames_per_sample==1)
-            if combine_frames:
-                indices_to_copy=np.zeros(int(2*data_amount/frames_per_sample)+1,dtype=int)
-                ind_i = 0
-                #Array to copy blocks of data into
-                temp_i = 0
-                i = 0
-                #label of continous set of matching data
-                cur_label = labels[0]
-                while i < data_amount:
-                    #Find the end of the continous set of matching data
-                    start = i
-                    while labels[i]==cur_label:
-                        i+=1
-                        if i == data_amount:
-                            break
-                    end = i
-                    #next set's label (if not at end)
-                    if not i == len(labels):
-                        cur_label=labels[i]
-                    #Add block of continous data to new dataset
-                    if end-start >= frames_per_sample and labels[start] >= 0:
-                        indices_to_copy[ind_i]=start
-                        indices_to_copy[ind_i+1]=end
-                        ind_i+=2
-                        labels[temp_i:temp_i+end-start]=labels[start:end]
-                        temp_i+=end-start
-                #trim to new size
-                labels=labels[:temp_i]
-                indices_to_copy=indices_to_copy[:ind_i]
-                    
-            with h5py.File(out_file_path,'w') as wf:
-                wf.create_dataset("/frames",shape=((len(labels),)+frame_shape),dtype='float32')
-                wf.create_dataset("/labels",data=labels)
-                cur_i = 0
-                for j in range(0,len(indices_to_copy),2):
-                    start = indices_to_copy[j]
-                    end = indices_to_copy[j+1]
-                    wf["/frames"][cur_i:cur_i+(end-start)]=(f["/frames/raw"][start:end])[...,None]
-                    cur_i+=end-start
-                    print("Processing File: [",(j/len(indices_to_copy)),"%]",end='\r')
-                #del data
-                del labels
